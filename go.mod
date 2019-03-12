ed3eff7dc4a16476127c3c62a1b52aea4dc5508f
Micah Young <myoung@pivotal.io>
2019-03-12 10:23:57 -0400

    Use go modules
    -Refactor update script to not use temp file

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/micahyoung/minio-buildpack
+
+require (
+	github.com/blang/semver v3.5.1+incompatible
+	github.com/cloudfoundry/libbuildpack v0.0.0-20190307214535-cd9ee704eaa2
+	github.com/krishicks/yaml-patch v0.0.10 // indirect
+	github.com/onsi/ginkgo v1.8.0
+	github.com/onsi/gomega v1.4.3
+)
--- scripts/update_minio.sh
+++ scripts/update_minio.sh
@@ -5,13 +5,7 @@ set -o nounset
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
-go get github.com/krishicks/yaml-patch/...
-
-go build -o yaml_patch_linux src/github.com/krishicks/yaml-patch/cmd/yaml-patch/*.go
-
-tmpdir=$(mktemp -d)
-tmpops=$(mktemp $tmpdir/XXX)
-trap "rm -r $tmpdir" EXIT
+go install github.com/krishicks/yaml-patch
 
 minio_url_prefix="https://dl.minio.io/server/minio/release/linux-amd64"
 mc_url_prefix="https://dl.minio.io/client/mc/release/linux-amd64"
@@ -25,7 +19,9 @@ minio_sha256="${minio_shasum[0]}"
 mc_url="$mc_url_prefix/archive/${mc_shasum[1]}"
 mc_sha256="${mc_shasum[0]}"
 
-cat > $tmpops <<EOF
+original_manifest_content=$(cat manifest.yml)
+
+ops_file_content=$(cat <<EOF
 - op: replace
   path: /dependencies/name=minio/uri
   value: "$minio_url#suffix=.sh"
@@ -39,5 +35,6 @@ cat > $tmpops <<EOF
   path: /dependencies/name=mc/sha256
   value: $mc_sha256
 EOF
+)
 
-yaml-patch --ops-file $tmpops < manifest.yml > manifest.yml.new
+yaml-patch --ops-file <(echo "$ops_file_content") < <(echo "$original_manifest_content") > manifest.yml
