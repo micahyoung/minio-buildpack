111dc4f8756e37c25cf84a594980624829a146c4
Micah Young <myoung@pivotal.io>
2019-04-30 09:33:59 -0400

    WIP Windows

--- scripts/update_minio.sh
+++ scripts/update_minio.sh
@@ -7,14 +7,17 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 go install github.com/krishicks/yaml-patch
 
-minio_url_prefix="https://dl.minio.io/server/minio/release/linux-amd64"
+minio_url_prefix="https://dl.minio.io/server/minio/release"
 mc_url_prefix="https://dl.minio.io/client/mc/release/linux-amd64"
 
-declare -a minio_shasum=(`curl -s $minio_url_prefix/minio.sha256sum`)
+declare -a minio_linux_shasum=(`curl -s $minio_url_prefix/linux-amd64/minio.sha256sum`)
+declare -a minio_windows_shasum=(`curl -s $minio_url_prefix/windows-amd64/minio.exe.sha256sum`)
 declare -a mc_shasum=(`curl -s $mc_url_prefix/mc.sha256sum`)
 
-minio_url="$minio_url_prefix/archive/${minio_shasum[1]}"
-minio_sha256="${minio_shasum[0]}"
+minio_linux_url="$minio_url_prefix/linux-amd64/archive/${minio_linux_shasum[1]}"
+minio_linux_sha256="${minio_linux_shasum[0]}"
+minio_windows_url="$minio_url_prefix/windows-amd64/archive/${minio_windows_shasum[1]}"
+minio_windows_sha256="${minio_windows_url[0]}"
 
 mc_url="$mc_url_prefix/archive/${mc_shasum[1]}"
 mc_sha256="${mc_shasum[0]}"
@@ -23,11 +26,17 @@ original_manifest_content=$(cat manifest.yml)
 
 ops_file_content=$(cat <<EOF
 - op: replace
-  path: /dependencies/name=minio/uri
-  value: "$minio_url#suffix=.sh"
+  path: /dependencies/name=minio-linux/uri
+  value: "$minio_linux_url#suffix=.sh"
 - op: replace
-  path: /dependencies/name=minio/sha256
-  value: $minio_sha256
+  path: /dependencies/name=minio-linux/sha256
+  value: $minio_linux_sha256
+- op: replace
+  path: /dependencies/name=minio-windows/uri
+  value: "$minio_windows_url#suffix=.sh"
+- op: replace
+  path: /dependencies/name=minio-windows/sha256
+  value: $minio_windows_sha256
 - op: replace
   path: /dependencies/name=mc/uri
   value: "$mc_url#suffix=.sh"
--- src/minio/supply/supply.go
+++ src/minio/supply/supply.go
@@ -46,18 +46,29 @@ type Supplier struct {
 func (s *Supplier) Run() error {
 	s.Log.BeginStep("Supplying minio")
 
-	dep := libbuildpack.Dependency{Name: "minio", Version: "latest"}
-	depMinio := filepath.Join(s.Stager.DepDir(), "minio")
-	if err := s.Installer.InstallDependency(dep, depMinio); err != nil {
-		return err
-	}
-	dep = libbuildpack.Dependency{Name: "mc", Version: "latest"}
-	depMc := filepath.Join(s.Stager.DepDir(), "mc")
-	if err := s.Installer.InstallDependency(dep, depMc); err != nil {
+	minioLinuxDep := libbuildpack.Dependency{Name: "minio-linux", Version: "latest"}
+	depMinioLinuxPath := filepath.Join(s.Stager.DepDir(), "minio-linux")
+	if err := s.Installer.InstallDependency(minioLinuxDep, depMinioLinuxPath); err != nil {
 		return err
 	}
 
-	if err := os.Chmod(depMinio, 0755); err != nil {
+	minioWindowsDep := libbuildpack.Dependency{Name: "minio-windows", Version: "latest"}
+	depMinioWindowsPath := filepath.Join(s.Stager.DepDir(), "minio-windows")
+	if err := s.Installer.InstallDependency(minioWindowsDep, depMinioWindowsPath); err != nil {
+		return err
+	}
+
+	mcLinuxDep := libbuildpack.Dependency{Name: "mc", Version: "latest"}
+	depMc := filepath.Join(s.Stager.DepDir(), "mc")
+	if err := s.Installer.InstallDependency(mcLinuxDep, depMc); err != nil {
+		return err
+	}
+
+	if err := os.Chmod(depMinioLinuxPath, 0755); err != nil {
+		return err
+	}
+
+	if err := os.Chmod(depMinioWindowsPath, 0755); err != nil {
 		return err
 	}
 
@@ -65,7 +76,11 @@ func (s *Supplier) Run() error {
 		return err
 	}
 
-	if err := s.Stager.AddBinDependencyLink(depMinio, "minio"); err != nil {
+	if err := s.Stager.AddBinDependencyLink(depMinioLinuxPath, "minio"); err != nil {
+		return err
+	}
+
+	if err := s.Stager.AddBinDependencyLink(depMinioWindowsPath, "minio.exe"); err != nil {
 		return err
 	}
 
